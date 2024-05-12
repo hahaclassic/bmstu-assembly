c1dfb2352b8a512826583f25189bf8b5aeba0974
hahaclassic <gavrilyukvlad221@gmail.com>
2024-05-13 02:19:31 +0300

    added lab 10

--- lab_10/find_root.c
+++ lab_10/find_root.c
@@ -27,7 +27,7 @@ double function(double x)
     return res;
 }
 
-int find_root(double a, double b, size_t iterations, double *result) 
+int find_root(double a, double b, int iterations, double *result) 
 {   
     double x = 0, a_val, b_val, x_val = 0;
 
@@ -35,7 +35,7 @@ int find_root(double a, double b, size_t iterations, double *result)
         return EXIT_FAILURE;
     }
 
-    for (size_t i = 0; i < iterations; i++) 
+    for (int i = 0; i < iterations; i++) 
     {
         a_val = function(a);
         b_val = function(b);
@@ -62,10 +62,6 @@ int find_root(double a, double b, size_t iterations, double *result)
         );
         x_val = function(x);
 
-        printf("a: %.20lf, a_val: %.20lf,\n\
-b: %.20lf, b_val: %.20lf,\n\
-x: %.20lf, x_val: %.20lf\n\n\n", a, a_val, b, b_val, x, x_val);
-
         if (a_val * x_val < 0) {
             b = x;
         } else if (b_val * x_val < 0) {
@@ -79,69 +75,3 @@ x: %.20lf, x_val: %.20lf\n\n\n", a, a_val, b, b_val, x, x_val);
     return EXIT_SUCCESS; 
 }
 
-int main()
-{
-    double a, b;
-    int count;
-
-    printf("Enter a: ");
-    int input = scanf("%lf", &a);
-    if (input != 1)
-    {
-        printf("[ERR]: error while reading float num\n");
-        return EXIT_FAILURE;
-    }
-    printf("Enter b: "); 
-    input = scanf("%lf", &b);
-    if (input != 1)
-    {
-        printf("[ERR]: error while reading float num\n");
-        return EXIT_FAILURE;
-    } 
-    printf("Enter num of interations: ");
-    input = scanf("%d", &count);
-    if (input != 1 || count <= 0)
-    {
-        printf("[ERR]: error while reading num of iterations\n");
-        return EXIT_FAILURE;
-    } 
-    if (a > b) {
-        double temp;
-        temp = a;
-        a = b;
-        b = temp;
-    }
-
-    double result;
-    int rc = find_root(a, b, count, &result);
-    if (!rc) {
-        printf("root not found.\n");
-    } else {
-        printf("root in [%lf, %lf]: %.20lf\n", a, b, result);
-    }
-
-    return EXIT_SUCCESS;
-}
-
-
-// __asm__ (
-//     "fld %2\n\t"
-//     "fld %3\n\t"
-//     "fmulp\n\t"
-//     "ftst\n\t"
-//     "fstsw ax\n\t"
-//     "sahf\n\t"
-//     "ja set_a\n\t"
-//     "set_b:\n\t"
-//     "   fld %4\n\t"
-//     "   fstp %1\n\t"
-//     "   jmp exit\n\t"
-//     "set_a:\n\t"
-//     "   fld %4\n\t"
-//     "   fstp %0\n\t"
-//     "exit:\n\t"
-//     "   fnop\n\t"          
-//     : "=m" (a), "=m" (b)                  
-//     : "m" (x_val), "m" (a_val), "m" (x)
-//     : "rax"
-// );
--- lab_10/main.asm
+++ lab_10/main.asm
@@ -5,6 +5,12 @@ global main
 %define GTK_WIN_WIDTH 700
 %define GTK_WIN_HEIGHT 450
 
+extern printf
+extern atoi
+extern sscanf
+extern sprintf
+extern find_root
+
 extern exit
 extern gtk_init
 extern gtk_main
@@ -19,16 +25,57 @@ extern gtk_window_set_position
 extern gtk_settings_get_default
 extern gtk_widget_set_size_request
 extern gtk_window_set_default_size
+extern gtk_entry_new
+extern gtk_button_new_with_label
+extern gtk_label_new
+extern gtk_label_set_text
+extern gtk_widget_set_halign
+extern gtk_entry_set_text
+extern gtk_widget_show_all
+extern gtk_container_add
+extern gtk_box_new
+extern gtk_orientable_set_orientation
+extern gtk_box_set_spacing
+extern gtk_entry_get_text
+extern g_ascii_strtod
+extern g_strdup_printf
+extern g_free
 
 section .bss
 window: resq 1
+label_start: resq 1 
+label_end: resq 1
+label_iterations: resq 1
+entry_start: resq 1
+entry_end: resq 1
+entry_iterations: resq 1
+label_result: resq 1
+output_result: resq 1
+button: resq 1
+box: resq 1
+
+section .data
+str_result: times 100 db 0
+result: dq 0
+str_ptr: dq 0
+
 section .rodata
 signal:
+.clicked: db "clicked", 0
 .destroy: db "destroy", 0
-title: db "My GTK+3 Window!", 0
+
+title: db "Root search by the chord method.", 0
+label_start_text: db "Enter start of the segment:", 0
+label_end_text: db "Enter end of the segment:", 0
+label_iterations_text: db "Enter number of iterations:", 0
+label_result_text: db "Result:", 0
+button_text: db "Find root", 0
+double_format: db "%lf", 0
+int_format: db "%d", 0
+
 section .text
 _destroy_window:
-jmp gtk_main_quit
+    jmp gtk_main_quit
 main:
     push rbp
     mov rbp, rsp
@@ -48,14 +95,209 @@ main:
     mov rdi, qword [ rel window ]
     mov rsi, GTK_WIN_POS_CENTER
     call gtk_window_set_position
+
+    ; Создание контейнера
+    call gtk_box_new
+    mov qword [rel box], rax
+
+    mov rdi, qword [rel box]
+    mov rsi, 1
+    call gtk_orientable_set_orientation
+
+    mov rdi, qword [rel window]
+    mov rsi, qword [rel box]
+    call gtk_container_add
+
+    ; Создание лэйблов и полей ввода
+    call gtk_entry_new
+    mov qword [rel entry_start], rax
+
+    call gtk_entry_new
+    mov qword [rel entry_end], rax
+
+    call gtk_entry_new
+    mov qword [rel entry_iterations], rax
+   
+    call gtk_label_new
+    mov qword [rel label_start], rax
+
+    call gtk_label_new
+    mov qword [rel label_end], rax
+
+    call gtk_label_new
+    mov qword [rel label_iterations], rax
+
+    call gtk_label_new
+    mov qword [rel label_result], rax
+
+    call gtk_label_new
+    mov qword [rel output_result], rax
+
+    ; Добавление текста в лэйблы
+    mov rdi, qword [rel label_start]
+    mov rsi, label_start_text
+    call gtk_label_set_text
+    mov rdi, qword [rel label_start]
+    mov rsi, 1
+    call gtk_widget_set_halign
+    
+    mov rdi, qword [rel label_end]
+    mov rsi, label_end_text
+    call gtk_label_set_text
+    mov rdi, qword [rel label_end]
+    mov rsi, 1
+    call gtk_widget_set_halign
+
+    mov rdi, qword [rel label_iterations]
+    mov rsi, label_iterations_text
+    call gtk_label_set_text
+    mov rdi, qword [rel label_iterations]
+    mov rsi, 1
+    call gtk_widget_set_halign
+
+    mov rdi, qword [rel label_result]
+    mov rsi, label_result_text
+    call gtk_label_set_text
+    mov rdi, qword [rel label_result]
+    mov rsi, 1
+    call gtk_widget_set_halign
+
+    mov rdi, qword [rel output_result]
+    mov rsi, str_result
+    call gtk_label_set_text
+    mov rdi, qword [rel output_result]
+    mov rsi, 1
+    call gtk_widget_set_halign
+
+    ; Создание кнопки
+    mov rdi, button_text
+    call gtk_button_new_with_label
+    mov qword [rel button], rax
+
+    ; Добавление виджетов в контейнер
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel label_start ]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel entry_start ]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel label_end ]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel entry_end ]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel label_iterations ]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel entry_iterations]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel label_result]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel output_result]
+    call gtk_container_add
+    mov rdi, qword [ rel box ]
+    mov rsi, qword [ rel button]
+    call gtk_container_add
+
+    ; Изменение расстояния между виджетами
+    mov rdi, qword [rel box]
+    mov rsi, 10
+    call gtk_box_set_spacing
+
+    ; Установка обработчика нажатия кнопки
+    mov rdi, qword [rel button]
+    mov rsi, signal.clicked
+    mov rdx, button_clicked_handler
+    xor rcx, rcx
+    xor r8d, r8d
+    xor r9d, r9d
+    call g_signal_connect_data
+
     mov rdi, qword [ rel window ]
     mov rsi, signal.destroy
-    mov rdx, _destroy_window
+    mov rdx, gtk_main_quit
     xor rcx, rcx
     xor r8d, r8d
     xor r9d, r9d
     call g_signal_connect_data
     mov rdi, qword [ rel window ]
-    call gtk_widget_show
+    call gtk_widget_show_all
     call gtk_main
     ret
+
+button_clicked_handler:
+    ; Обработчик события нажатия кнопки
+    ; Считываем данные из полей ввода
+    mov rdi, [entry_start]
+    call gtk_entry_get_text
+    mov rdi, rax
+    xor rsi, rsi
+    call g_ascii_strtod
+    movsd xmm0, [rsp]
+
+    mov rdi, [entry_end]
+    call gtk_entry_get_text
+    mov rdi, rax
+    xor rsi, rsi
+    call g_ascii_strtod
+    movsd xmm1, [rsp]
+
+    mov rdi, [entry_iterations]
+    call gtk_entry_get_text
+    call convert_to_int
+    mov rdi, rax
+
+    mov rsi, [result]
+
+    call find_root
+
+    mov rdi, qword [double_format]
+    movsd xmm0, [result]
+    mov rax, 1
+    call g_strdup_printf
+    mov [str_ptr], rax
+    ;call convert_to_str
+
+    mov rdi, qword [rel output_result]
+    mov rsi, [str_ptr]
+    call gtk_label_set_text
+    mov rdi, [str_ptr]
+    call g_free
+    ret
+
+
+; convert_to_double:
+;     sub rsp, 8 
+;     mov rdi, rax
+;     mov rsi, double_format
+;     lea rdx, [result]
+;     mov rax, 0 
+;     call sscanf
+;     add rsp, 8 
+;     ret
+
+convert_to_int:
+    sub rsp, 8
+    mov rdi, rax
+    call atoi
+    add rsp, 8
+    ret
+
+convert_to_str: 
+    sub rsp, 8
+    mov rdi, str_result
+    mov rsi, double_format
+    mov rdx, result
+    mov rax, 0
+    call sprintf
+    add rsp, 8
+    ret
+    
+fuck:
+    mov rdi, qword [rel output_result]
+    mov rsi, title
+    call gtk_label_set_text
